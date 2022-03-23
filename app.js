8edbabc65234be452b74f6b5dfe00c59f7c9c686
hmrvc <hmrvc@yahoo.com.tw>
2022-03-23 16:08:29 +0800

    專案初始化設定

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const routes = require('./routes')
+const app = express()
+
+const PORT = 3000
+
+app.engine('hbs', exphbs({ extname: 'hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(routes)
+app.listen(PORT, () => {
+  console.log(`app listening on localhost: ${PORT}`)
+})
