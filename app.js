cace0de7fc1b9626f2f477500c4718540afa7c95
hmrvc <hmrvc@yahoo.com.tw>
2022-03-24 14:49:00 +0800

    feat:增加 header&footer

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
 const flash = require('connect-flash')
+const handlebarsHelpers = require('./helpers/handlebars-helper')
 const routes = require('./routes')
 const app = express()
 const PORT = 3000
 const SESSION_SECRET = 'secret'
 
-app.engine('hbs', exphbs({ extname: 'hbs' }))
+app.engine('hbs', exphbs({ extname: 'hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
 app.use(flash())
