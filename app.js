609762c7fc9ce5c294707c89557f129971e7893c
hmrvc <hmrvc@yahoo.com.tw>
2022-03-24 08:23:46 +0800

    設定MySQL資料庫

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
 const app = express()
-
 const PORT = 3000
 
 app.engine('hbs', exphbs({ extname: 'hbs' }))
