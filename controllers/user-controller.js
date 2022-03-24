a2cbb2c020361fbb7b9a51c30b00c53ac58e971f
hmrvc <hmrvc@yahoo.com.tw>
2022-03-24 15:46:54 +0800

    feat: signup改寫成async/await

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,17 +6,21 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-      .then(hash => User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: hash
-      }))
-      .then(() => {
-        res.redirect('/signin')
-      })
-      .catch(err => next(err))
+  signUp: async(req, res, next) => {
+    try {
+      if (req.body.password !== req.body.passwordCheck) throw new Error('Password do not match!')
+      const user = await User.findOne({ where: { email: req.body.email } })
+      if (user) throw new Error('Email already exists!')
+      const hash = await bcrypt.hash(req.body.password, 10)
+      const newUser = await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: hash
+        })
+      res.redirect('/signin')
+    } catch(err){
+      next(err)
+    }
   },
   signInPage: (req, res, next) => {
     res.render('signin')
