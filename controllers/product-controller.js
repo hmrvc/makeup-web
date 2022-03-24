e2b2f776225af9b72b758f77d3a5a6df82bcf362
hmrvc <hmrvc@yahoo.com.tw>
2022-03-24 10:02:04 +0800

    feat: add products index page

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,6 +1,18 @@
+const { Product } = require('../models')
 const productController = {
   getProducts: (req, res) => {
-    return res.render('products')
+    return Product.findAll({
+      raw: true
+    }).then(products => {
+      const data = products.map(p => ({
+        ...p,
+        description: p.description ? p.description.substring(0, 50) : null
+      }))
+      return res.render('products', {
+        products: data
+      })
+
+    })
   }
 }
 
