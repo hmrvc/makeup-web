cf848ca51cabf351e65706e8cced5522f5a8f6d9
hmrvc <hmrvc@yahoo.com.tw>
2022-03-24 08:39:17 +0800

    feat:add index page

---
+++ controllers/product-controller.js
@@ -0,0 +1,7 @@
+const productController = {
+  getProducts: (req, res) => {
+    return res.render('products')
+  }
+}
+
+module.exports = productController
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('HI')
+//載入controller
+const productController = require('../controllers/product-controller')
+
+router.get('/products', productController.getProducts)
+router.use('/', (req, res) =>{
+  res.redirect('/products')
 })
 
 module.exports = router
