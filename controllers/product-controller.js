52fdb02d7a62acc4b0c2573f8e0f706294999b1e
hmrvc <hmrvc@yahoo.com.tw>
2022-03-25 11:16:30 +0800

    feat: 增加分頁功能

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,15 +1,24 @@
 const { Product } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 const productController = {
   getProducts: (req, res, next) => {
-    return Product.findAll({
+    const DEFAULT_LIMIT = 9
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
+    return Product.findAndCountAll({
+      limit, 
+      offset,
       raw: true
     }).then(products => {
-      const data = products.map(p => ({
+      const data = products.rows.map(p => ({
         ...p,
         description: p.description ? p.description.substring(0, 50) : null
       }))
       return res.render('products', {
-        products: data
+        products: data,
+        pagination: getPagination(limit, page, products.count)
       })
     })
     .catch(err => next(err))
