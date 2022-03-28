eb227dfd8041ae2254224cb1cb5bbf06f4793a66
hmrvc <hmrvc@yahoo.com.tw>
2022-03-28 09:01:13 +0800

    feat: 優化每次顯示10個分頁

--- helpers/pagination-helper.js
+++ helpers/pagination-helper.js
@@ -1,7 +1,11 @@
 const getOffset = (limit = 10, page = 1) => (page - 1) * limit
 const getPagination = (limit = 10, page = 1, total = 50) => {
   const totalPage = Math.ceil(total / limit)
-  const pages = Array.from({ length: totalPage }, (_, index) => index + 1)
+  // 總共資料數total 每頁顯示幾筆資料limit 目前頁面page 每次顯示幾個分頁 pagLimit
+  const pagLimit = 10 // 依次顯示10個分頁
+  const first = 1 // 第一頁
+  const last = totalPage // 最後一頁
+  const pages = Array.from({ length: totalPage }, (_, index) => index + 1).slice(page - 1, page + pagLimit - 1)
   const currentPage = page < 1 ? 1 : page > totalPage ? totalPage : page
   const prev = currentPage - 1 < 1 ? 1 : currentPage - 1
   const next = currentPage + 1 > totalPage ? totalPage : currentPage + 1
@@ -10,7 +14,9 @@ const getPagination = (limit = 10, page = 1, total = 50) => {
     totalPage,
     currentPage,
     prev,
-    next
+    next,
+    first,
+    last
   }
 }
 
